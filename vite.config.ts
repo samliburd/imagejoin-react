82c9ae8ca00cd280ef227939bd4d14d9dd10ee71
samliburd <liburdsam@gmail.com>
2026-07-09 10:23:26 +0100

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+          name: 'ImageJoin',
+          short_name: 'ImageJoin',
+          description: 'Tool to join two or more images using the Canvas API',
+          theme_color: '#2f3b45',
+      }
+  })],
 })
