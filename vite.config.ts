bee50ffa12dd09a98e17637974b0201a762437a6
samliburd <liburdsam@gmail.com>
2026-07-09 10:30:55 +0100

    updated pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,27 @@ export default defineConfig({
           short_name: 'ImageJoin',
           description: 'Tool to join two or more images using the Canvas API',
           theme_color: '#2f3b45',
+          icons: [{
+              "src": "pwa-64x64.png",
+              "sizes": "64x64",
+              "type": "image/png"
+          },
+              {
+                  "src": "pwa-192x192.png",
+                  "sizes": "192x192",
+                  "type": "image/png"
+              },
+              {
+                  "src": "pwa-512x512.png",
+                  "sizes": "512x512",
+                  "type": "image/png"
+              },
+              {
+                  "src": "maskable-icon-512x512.png",
+                  "sizes": "512x512",
+                  "type": "image/png",
+                  "purpose": "maskable"
+              }]
       }
   })],
 })
